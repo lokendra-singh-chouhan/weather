d87746bfd2b98faca512a437b1bda97f786fefd4
lokendrasingh.chouhan.29 <lokendrasingh.chouhan.29@gmail.com>
2023-05-15 17:02:02 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather';
+  title = 'calculator';
+  toshow="0"
+  curvalue=''
+ 
+  darkMode = false;
+  display(value:any){
+    this.curvalue =this.curvalue +value
+    this.toshow =this.curvalue
+  }
+  calculate(){
+    this.toshow=eval(this.curvalue)
+    this.curvalue=this.toshow
+  }
+  clr(){
+    this.toshow='0';
+    this.curvalue="";
+  }
+  del(){
+    this.curvalue=this.curvalue.slice(0,-1)
+  
+    this.toshow=this.curvalue
+    if(this.toshow==""){
+      this.toshow='0'
+      
+    
+    }
+
+  }
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
+  
+
 }
+
